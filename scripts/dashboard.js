069552902460e1e01f54309d7f6059d535a67131
Alain Kimbu <54691684+Rodern@users.noreply.github.com>
2021-12-08 17:41:35 +0100

    more

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,31 +1,36 @@
+var isOn = 'false'
+	$(".headerMenu").on('click', function() {
+		$('.stackPanel').css({'width': '0px','display': 'flex'}).animate({
+			width: "250px"
+		},
+		{
+			duration: 200,
+			easing: 'linear',
+			complete: function() {
+				isOn = 'true'
+			}
+		})
+	});
 
-    $(".headerMenu").on('click', function() {
-        $('.stackPanel').css({'width': '0px','display': 'flex'}).animate({
-            width: "250px"
-        },
-        {
-            duration: 200,
-            easing: 'linear'
-        })
-    });
-
-    $('.exitPanel').on('click', function() {
-        $('.stackPanel').animate({
-            width: "0px"
-        },
-        {
-            duration: 200,
-            easing: 'linear',
-            complete: function() {
-                $('.stackPanel').css({
-                    'display': '',
-                    'width': '250px'
-                });
-            }
-        })
-    });
+	$('.exitPanel').on('click', function() {
+		$('.stackPanel').animate({
+			width: "0px"
+		},
+		{
+			duration: 200,
+			easing: 'linear',
+			complete: function() {
+				$('.stackPanel').css({
+					'display': '',
+					'width': '250px'
+				});
+				isOn = 'false'
+			}
+		})
+	});
 
 
-    $('.panel_list li').on('click', function() {
-        alert('Hello There!!!');
-    });
+	$('.panel_list li').on('click', function() {
+		alert($('.stackPanel').css('display'));
+		
+	});
