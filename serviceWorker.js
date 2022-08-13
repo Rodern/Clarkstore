5f4107347e440ebbe3db76125dd9a6cb2381248d
Alain Kimbu <54691684+Rodern@users.noreply.github.com>
2022-08-13 19:33:45 +0100

    service worker touched

--- serviceWorker.js
+++ serviceWorker.js
@@ -13,7 +13,7 @@ var staticAssets = [
 self.addEventListener('install', installEvent => {
     installEvent.waitUntil(
         caches.open(cacheName).then(cache => {
-            cache.addAll(staticAssets);
+            //cache.addAll(staticAssets);
         })
     )
 });
